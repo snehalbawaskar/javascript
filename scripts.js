55f76772fd1dce61f644f0a26af179904c9fd6b4
snehal bawaskar <snehalbawaskar97@gmail.com>
2021-04-24 16:21:55 +0530

    fisrt jsdemo

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//function changeBacground(){
+//    document.body.style.backgroundColor="yellowgreen"
+//}
+
+let button1=document.getElementById("changeColor")
+button1.addEventListener('click',()=>{
+    document.body.style.backgroundColor="red"
+})
