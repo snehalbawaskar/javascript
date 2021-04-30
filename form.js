0764fb8ed61929c00ed005340f1681a3c0a81def
snehal bawaskar <snehalbawaskar97@gmail.com>
2021-04-30 18:32:55 +0530

    firstcommite

---
+++ form.js
@@ -0,0 +1,10 @@
+
+<script type="text/javascript">
+function chkblnk(eid,errid)
+{
+var x=document.getElementById(eid).value;
+if(x=="")
+{
+document.getElementById(errid).innerHTML="pls fill this field";
+}
+else
